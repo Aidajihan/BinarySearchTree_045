59562ab9b0e8ddcd1aa90a29a0a3a899a45ba8b8
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-12-26 18:03:41 +0700

    membuat menu

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -117,7 +117,17 @@ namespace BinarySearchTree
         }
         static void Main(string[] args)
         {
-            
+            BinaryTree x = new BinaryTree();
+            while(true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2. Perform inorder transversal");
+                Console.WriteLine("3. Perform preorder transversal");
+                Console.WriteLine("4. Perform postorder transversal");
+                Console.WriteLine("5. Exit");
+                Console.Write("\n Enter your choice (1-5) : ");
+            }
         }
     }
 
