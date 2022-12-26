dfb3f23e7ce039f881d898d4a35b621c906b09a0
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-12-26 17:20:40 +0700

    membuat class node

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
-    internal class Program
+    class node
     {
+        public string info;
+        public node lchild;
+        public node rchild;
+
+        //constructor for the node class
+
+        public node(string i, node l, node r)
+        {
+            info = i;
+            lchild = l;
+            rchild = r;
+        }
+    }
+
+    
+    
+    
+       
+    class Program
+    {
+        /* A node  class consi
+     
+     */
+       
         static void Main(string[] args)
         {
         }
